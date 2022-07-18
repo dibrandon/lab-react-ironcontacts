e4d209d62ff28e3c309e1a5ca77055ce97750333
alejandro_diaz <diazbrandonalejandro@gmail.com>
2022-07-18 05:09:09 +0200

    lab iteracion 1 hecha

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json"
+import { useState } from 'react';
+const getFiveArr = contacts.splice(0, 5)
 
 function App() {
+const [contacts, setContacts] = useState(getFiveArr)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th> Picture </th>
+            <th> Name </th>
+            <th> Popularity </th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <td>
+                  <img
+                    src={contact.pictureUrl}
+                    width="100px"
+                    height="120px"
+                    alt="Actor"
+                  />
+                </td>
+                <td> {contact.name} </td>
+                <td> {contact.popularity}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
